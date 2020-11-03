5a70a5985174ff9545a1dd7f39a3b22a0e881c67
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-04 01:40:03 +0200

    update lectures 2-nd part: new file

---
+++ W7D1-2.php
@@ -0,0 +1,29 @@
+<?php
+// W7D1
+// (1:15:)
+
+var_dump($_POST);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST">
+        <input name="input_one">
+        <input name="input_two">
+
+        <button type="submit">Save</button>
+
+    </form>
+</body>
+
+</html>
