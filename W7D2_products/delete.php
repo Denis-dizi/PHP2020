2701ebba4fc5a28923410025f1f5debea12b668b
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-05 22:32:14 +0200

    updated addresses and timecodes

--- W7D2_products/delete.php
+++ W7D2_products/delete.php
@@ -1,16 +1,14 @@
 <?php
 // W7D2
-// 
+// (0:52:)
 $dbhost = "localhost:3306";
 $dbuser = "root";
 $dbpass = ""; //root
 $dbname = "shop-i";
 $dbConnection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-
+// (0:54:)
 $id = $_GET["id"];
 $sql = "DELETE FROM products WHERE id=$id";
 $dbConnection->query($sql);
 
-Header('Location: /PHP2020 - RCS/products/list.php');
-
-?>
+Header('Location: /PHP2020_RCS/W7D2_products/list.php');
