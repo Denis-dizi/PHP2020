f8ce21d789f5af6f34ac89a66a710de54bb28e18
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-12 16:35:43 +0200

    update (0:50:) DB call working

--- W8D1_mvc/controllers/listController.php
+++ W8D1_mvc/controllers/listController.php
@@ -2,18 +2,19 @@
 //  (0:21:)
 echo "listController.php - test List controller<br>";
 require_once __DIR__ . "/../views/listView.php"; //(0:31:)
-// require_once __DIR__ . "/../models/listModel.php";
+require_once __DIR__ . "/../models/listModel.php"; //(0:47:)
 // require_once __DIR__ . "/../components/modifyForm.php";
 
-// $model = new listModel();
-// $products = $model->getAll();
-
-// var_dump($products);
+//(0:48:)
+$model = new listModel();
+$products = $model->getAll();
 
 // (0:33:)
-$view = new listView();
 // $view = new listView($products);
+$view = new listView();
 $view->html();
+
+var_dump($products); //(0:50:)
 
 // if (isset($_GET["action"]) && $_GET["action"] === "modify") {
 //     if (isset($_GET["product_id"])) {
--- W8D1_mvc/models/listModel.php
+++ W8D1_mvc/models/listModel.php
@@ -1,38 +1,44 @@
 <?php
-require_once __DIR__ . "/../W7D3/DB_wrapper.php";
-    class listModel {
+// (0:42:)
+require_once __DIR__ . "/../../W7D3/DB_wrapper.php"; //(0:44:)
+class listModel
+{
+    // (0:43:)
+    public function getAll()
+    {
+        $sql = "SELECT * FROM products";
+        $response = DB::run($sql)->fetch_all(MYSQLI_ASSOC);
 
-        public function getAll() {
-            $sql = "SELECT * FROM products";
-            $response = DB::run($sql)->fetch_all(MYSQLI_ASSOC);
+        return $response;
+    }
 
-            return $response;
-        }
+    public function deleteById($id)
+    {
+        $sql = "DELETE FROM products WHERE id=$id";
+        DB::run($sql);
+    }
 
-        public function deleteById($id) {
-            $sql = "DELETE FROM products WHERE id=$id";
-            DB::run($sql);
-        }
+    public function getById($id)
+    {
+        $sql = "SELECT * FROM products WHERE id=$id";
+        $response = DB::run($sql);
 
-        public function getById($id) {
-            $sql = "SELECT * FROM products WHERE id=$id";
-            $response = DB::run($sql);
-
-            if ($response->num_rows ===0) {
-                return [];
-            } else{
-                return $response->fetch_assoc();
-            }
-        }
-
-        public function updateById($id, $name, $price) {
-            $sql = "INSERT products SET name = '$name', price=$price WHERE id=$id ";
-            DB::run($sql);
-        }
-        
-        public function insertNew($name, $price) {
-            $sql = "INSERT INTO products (name, price) VALUES ('$name', $price)";
-            DB::run($sql);
+        if ($response->num_rows === 0) {
+            return [];
+        } else {
+            return $response->fetch_assoc();
         }
     }
-?>
+
+    public function updateById($id, $name, $price)
+    {
+        $sql = "INSERT products SET name = '$name', price=$price WHERE id=$id ";
+        DB::run($sql);
+    }
+
+    public function insertNew($name, $price)
+    {
+        $sql = "INSERT INTO products (name, price) VALUES ('$name', $price)";
+        DB::run($sql);
+    }
+}
