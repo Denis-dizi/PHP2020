554f7038ad58d7de9641508d66b4f2647707fcce
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-13 16:43:27 +0200

    update (1:27:). Login veryfication. Works

--- W8D2_sess/controllers/loginController.php
+++ W8D2_sess/controllers/loginController.php
@@ -2,43 +2,49 @@
 // (0:46:)
 echo "loginController.php - test<br>";
 
-// require_once __DIR__ . "/../components/userForm.php";
-// require_once __DIR__ . "/PHP2020_RCS/W7D3/DB_wrapper.php";
+// (1:13:)
+require_once __DIR__ . "/../components/userForm.php"; // (1:13:)
+require_once __DIR__ . "/../DB_wrapper_ses.php"; // (1:17:)
 
-// if (!empty($_POST["email"])) {
-//     $email = $_POST["email"];
+// (1:14:)
+if (!empty($_POST["email"])) {
+    $email = $_POST["email"];
 
-//     $sql = "SELECT * FROM users WHERE email = '$email'";
+    $sql = "SELECT * FROM users WHERE email = '$email'";
 
-//     $response = DB::run($sql)-> fetch_assoc();
+    $response = DB::run($sql)-> fetch_assoc();
 
-//     // var_dump($response);
+    var_dump($response);
 
-//     if($response) {
-//         if (!empty($_POST["password"])) {
-//             $isValidPassword = password_veryfy(
-//                 $_POST["password"],
-//                 $response["password"]
-//             );
+// (1:18:) interesting experiments!!!
+    if($response) {
+        // (1:19:) do DB have an user, is the passwrd valid:
+        // (1:22:) Only for hashed in DB passwords.
+        if (!empty($_POST["password"])) {
+            $isValidPassword = password_verify(
+                $_POST["password"],
+                $response["password"]
+            );
+        //    (1:23:)
+            if ($isValidPassword) {
+                echo "Congrats!! You're inn.";
+                // session_start();
+                // $_SESSION["id"] = $response["email"];
+                // Header("Location: /mvc/?page=list")
+            } else {
+                echo "Invalid password";
+            }
+        } else {
+            echo "Password not set";
+        }
+    } else {
+        echo "User with email: '$email' does not exists";
+    }
+}
 
-//             if ($isValidPassword) {
-//                 session_start();
-//                 $_SESSION["id"] = $response["email"];
-//                 Header("Location: /mvc/?page=list")
-//             } else {
-//                 echo "Invalid password";
-//             }
-
-//         } else {
-//             echo "Password not set";
-//         }
-
-//     } else {
-//         echo "User with email: '$email' does not exists";
-//     }
-// }
-// $form = new UserForm();
-// $form->setBtnText("Login");
-// $form->html();
+// (1:13:)
+$form = new UserForm();
+$form->setBtnText("Login");
+$form->html();
 
 ?>
