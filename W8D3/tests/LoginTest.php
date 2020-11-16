869efd52d736a3c808f2f5f3e8a08a18814ee79b
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-16 02:12:33 +0200

    update lecture W8D3 (1:10:). LoginTest.php. Works.

---
+++ W8D3/tests/LoginTest.php
@@ -0,0 +1,19 @@
+<?php
+// (0:42:)
+// name of class must be same as file name:
+
+use PHPUnit\Framework\TestCase;
+class LoginTest extends TestCase {
+    //(0:53:)
+    public function testExample() {
+        $this->assertEquals(3, 3);
+        // $this->assertEquals(3, 1); // (1:11:)
+    }
+
+    // (1:13:)
+    public function testLoginController() {
+        
+    }
+}
+// (0:54:)
+// ./vendor/phpunit/phpunit/phpunit ./PHP2020_RCS/W8D3/tests
